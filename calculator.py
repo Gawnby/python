988633a27f370474ce74bd4096720d1e45b00916
Gawnby <gombaspeteer@gmail.com>
2016-10-18 21:54:10 +0200

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,24 @@
+num1=int (input("Enter a number (or a letter to exit) "))
+func=input ("Enter an operation: ")
+num2=int (input("Enter another number: "))
+
+if func=="+":
+  ans=num1+num2
+  print (ans) 
+else:
+  pass
+if func=="-":
+  ans=num1-num2
+  print (ans)
+else:
+  pass
+if func=="*":
+  ans=num1*num2
+  print (ans)
+else:
+  pass
+if func=="/":
+  ans=num1/num2
+  print (ans)
+else:
+  pass
