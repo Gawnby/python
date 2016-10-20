5cef7111ec78107f29f7de9a3d3146f2a4a8d1f5
Gawnby <gombaspeteer@gmail.com>
2016-10-20 13:51:15 +0200

    ujra

--- tictactoe.py
+++ tictactoe.py
@@ -1,31 +1,35 @@
 #Tic Tac Toe
 import random
-
-board = ["0"," 1"," 2"," 3"," 4"," 5"," 6"," 7"," 8"," 9","10","11","12","13","14","15","16"]
-
-
+board=[1]*16
+for field in range(0,len(board)):
+    if(len(str(field))==1):
+      board[field]=' '+str(field)
+    else:
+      board[field]=field
 def drawBoard(board):
     #this function draw the board if that was passed
+  width=4
+  height=4
+  print('-------------------------')
+  for slice in range(0,height):
+      print('|',end='')
+      for field in range(slice*width,slice*width+width):  
+        print('     |',end='')
+      print('\n',end='')
+      print('| ',end='')
+      for field in range(slice*width,slice*width+width):
+        print(' '+str(board[field])+' | ',end='')
+      print('\n',end='')
+      print('|',end='')
+      for field in range(slice*width,slice*width+width):
+        print('     |',end='')
+      print('\n',end='')
+      print('-------------------------')
 
-    print('    |    |    |')
-    print(' ' + board[13] + ' | ' + board[14] + ' | ' + board[15] + ' | ' + board[16])
-    print('    |    |    |')
-    print('-------------------')
-    print('    |    |    |')
-    print(' ' + board[9] + ' | ' + board[10] + ' | ' + board[11] + ' | ' + board[12])
-    print('    |    |    |')
-    print('-------------------')
-    print('    |    |    |')
-    print(' ' + board[5] + ' | ' + board[6] + ' | ' + board[7] + ' | ' + board[8])
-    print('    |    |    |')
-    print('-------------------')
-    print('    |    |    |')
-    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3] + ' | ' + board[4])
-    print('    |    |    |')
+
 drawBoard(board)
 
 hit=" x"
-
 while True:
     field = eval(input("Select a field: "))
     if board[field] !=' x' and board[field] !=' x':
@@ -38,4 +42,4 @@ while True:
     else: 
         hit =' x'
 
-
+       
